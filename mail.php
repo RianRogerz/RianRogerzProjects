478cbc7d1d46b66a53a101a8e889079d3ef98253
RianRog <rjrogerz@gmail.com>
2022-03-11 18:02:17 +0000

    Website built. starting preliminary task next

---
+++ mail.php
@@ -0,0 +1,20 @@
+<?php
+
+$name = $_POST['name'];
+$email= $_POST['email'];
+$message= $_POST['message'];
+$number= $_POST['number'];
+
+$to = "rjrogerz@gmail.com";
+
+$subject = "Mail From Rog Solutions";
+$txt ="Name = ". $name . "\r\nEmail = " . $email . "\r\nNumber = " . $number . "\r\nMessage = " . $message;
+$headers = "From: noreply@rogsolutions.co.uk" . "\r\n" .
+"CC: rjrogerz@rogsolutions.co.uk";
+if($email!=NULL){
+    mail($to,$subject,$txt,$headers);
+}
+
+header("Location:index.html#contact");
+
+?>
